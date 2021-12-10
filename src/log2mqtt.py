7423898fca9a6c7cc4f2fb41b24771e07e895d55
Paulo Machado <paulo.machado@qpro.com.br>
2021-12-10 15:20:10 -0300

    retry, qos and retain when publishing

--- src/log2mqtt.py
+++ src/log2mqtt.py
@@ -11,13 +11,20 @@ class Log2MQTT:
         self.mqtt_client = mqtt.Client()
         self.mqtt_client.connect(host)
 
-    def log(self, data: dict):
+    def log(self, data: dict, retries=3):
         """Send the log to the MQTT broker."""
         log_list = [f"{key[:4]}: {value}" for key, value in data.items()]
 
-        result = self.mqtt_client.publish(self.mqtt_topic, "\n".join(log_list))
-        if result.rc != mqtt.MQTT_ERR_SUCCESS:
-            print(f"Error sending MQTT message. Return code: {result.rc}")
+        if self.mqtt_client.is_connected():
+            result = self.mqtt_client.publish(
+                self.mqtt_topic, "\n".join(log_list), qos=1, retain=True)
+            if result.rc != mqtt.MQTT_ERR_SUCCESS:
+                print(f"Error sending MQTT message. Return code: {result.rc}")
+        elif retries > 0:
+            self.mqtt_client.reconnect()
+            self.log(data, retries - 1)
+        else:
+            print(f"Error connecting to MQTT broker. Retries: {retries}")
 
 
 if __name__ == "__main__":
